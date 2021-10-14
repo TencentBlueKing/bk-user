117c39e683e4860849505a350e02db6e767aa9f3
IMBlues <bluesedenyu@gmail.com>
2021-10-14 16:20:53 +0800

    fix: 修正权限中心注册脚本环境变量 APP_ID & APP_TOKEN -> BK_APP_CODE & BK_APP_SECRET

--- src/api/bin/migrate_iam.sh
+++ src/api/bin/migrate_iam.sh
@@ -2,5 +2,5 @@
 echo "Start to migrate bkiam"
 for filename in bkuser_core/bkiam/migrations/*.json; do
   echo ">>>>>>>>>>>>> migrating $filename ..."
-  python bkuser_core/bkiam/migrations/do_migrate.py -f "$filename" -a ${APP_ID} -s ${APP_TOKEN} -t ${BK_IAM_V3_INNER_HOST}
+  python bkuser_core/bkiam/migrations/do_migrate.py -f "$filename" -a ${BK_APP_CODE} -s ${BK_APP_SECRET} -t ${BK_IAM_V3_INNER_HOST}
 done
