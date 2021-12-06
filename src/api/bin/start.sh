1098908508960a4f491671ebdf09ff3e5ec6fdc0
IMBlues <bluesedenyu@gmail.com>
2021-12-06 16:31:26 +0800

    fix: 补上国际化步骤

--- src/api/bin/start.sh
+++ src/api/bin/start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+python manage.py compilemessages
+
 gunicorn wsgi -w 8 --threads 2 --max-requests 1024 --max-requests-jitter 50 --worker-class gevent -b :8000 --access-logfile - --error-logfile - --access-logformat '[%(h)s] %({request_id}i)s %(u)s %(t)s "%(r)s" %(s)s %(D)s %(b)s "%(f)s" "%(a)s"'
--- src/login/bin/start.sh
+++ src/login/bin/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 python manage.py collectstatic
+python manage.py compilemessages
 
 command="gunicorn wsgi -w 16 --timeout 150 -b 0.0.0.0:5000 -k gevent --max-requests 1024 --access-logfile '-' --access-logformat '%(h)s %(l)s %(u)s %(t)s \"%(r)s\" %(s)s %(b)s \"%(f)s\" \"%(a)s\" in %(L)s seconds' --log-level INFO --log-file=-"
 
