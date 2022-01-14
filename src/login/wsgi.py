db6f2a744ea03c8fa5a867e44475303a37d0e063
Blues Yu <bluesedenyu@gmail.com>
2022-01-14 16:27:07 +0800

    chore: support custom command & args for saas

--- src/login/wsgi.py
+++ src/login/wsgi.py
@@ -15,6 +15,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bklogin.config.overlays.prod")
 
 application = Cling(get_wsgi_application())
