071eec38d83f775f43d0ba6abbd90ec451cfe8d3
Blues Yu <bluesedenyu@gmail.com>
2022-03-22 11:03:52 +0800

    fix: 登录跳转路径添加尾/作为默认值，确保二进制部署时能够正确跳转

--- src/api/bkuser_core/user_settings/constants.py
+++ src/api/bkuser_core/user_settings/constants.py
@@ -23,7 +23,6 @@ class SettingsEnableNamespaces(AutoLowerEnum):
         (GENERAL, "通用"),
         (PASSWORD, "密码"),
         (CONNECTION, "连接"),
-        # revert #300
         (FIELDS, "字段"),
     )
 
