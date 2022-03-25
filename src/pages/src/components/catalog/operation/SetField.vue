af47a83fa14201f9c0e036b8ef8a662a60ce4da5
v_yutyi <v_yutyi@tencent.com>
2022-03-25 15:50:40 +0800

    fix: 修复footer显示样式/国际化

--- src/pages/src/components/catalog/operation/SetField.vue
+++ src/pages/src/components/catalog/operation/SetField.vue
@@ -97,8 +97,8 @@
         </div>
         <div class="content catalog-setting-step" v-show="expandUser">
           <div class="user-directory">
-            <p>蓝鲸用户管理字段</p>
-            <p>对应{{catalogType}}目录字段</p>
+            <p>{{$t('蓝鲸用户管理字段')}}</p>
+            <p>{{$t('对应')}} {{catalogType}} {{$t('目录字段')}}</p>
           </div>
           <div class="user-content">
             <div class="user-item">
@@ -138,7 +138,7 @@
         <div class="content catalog-setting-step" v-show="expandCustom">
           <div class="user-directory">
             <p>{{$t('蓝鲸用户管理字段')}}</p>
-            <p>{{$t('对应')}}{{catalogType}}{{$t('目录字段')}}</p>
+            <p>{{$t('对应')}} {{catalogType}} {{$t('目录字段')}}</p>
           </div>
           <addCustomField :type="type" :custom-field="customField" @upAddFieldList="getAddFieldList" />
           <setCustomField
