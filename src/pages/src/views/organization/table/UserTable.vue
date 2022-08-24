e0f5a569427a4a5be000c1ae660161b638384c98
v_yutyi <v_yutyi@tencent.com>
2022-08-24 10:28:41 +0800

    fix: 枚举字段展示问题

--- src/pages/src/views/organization/table/UserTable.vue
+++ src/pages/src/views/organization/table/UserTable.vue
@@ -267,7 +267,7 @@ export default {
               if (this.$i18n.locale === 'en') {
                 return value;
               }
-              return this.statusMap[options[i].id];
+              return this.statusMap[key][i];
             }
           }
         } else {
