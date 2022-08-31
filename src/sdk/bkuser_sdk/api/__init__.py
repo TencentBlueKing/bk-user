80e1f3bcc73c60ffe9ee20f9c005351584db5491
wklken <wklken@gmail.com>
2022-09-01 00:04:08 +0800

    refactor(saas/sdk): remove useless apis

--- src/sdk/bkuser_sdk/api/__init__.py
+++ src/sdk/bkuser_sdk/api/__init__.py
@@ -7,10 +7,6 @@ from bkuser_sdk.api.batch_api import BatchApi
 from bkuser_sdk.api.categories_api import CategoriesApi
 from bkuser_sdk.api.departments_api import DepartmentsApi
 from bkuser_sdk.api.dynamic_fields_api import DynamicFieldsApi
-from bkuser_sdk.api.edges_api import EdgesApi
-from bkuser_sdk.api.healthz_api import HealthzApi
-from bkuser_sdk.api.pong_api import PongApi
 from bkuser_sdk.api.profiles_api import ProfilesApi
 from bkuser_sdk.api.settings_api import SettingsApi
 from bkuser_sdk.api.shortcuts_api import ShortcutsApi
-from bkuser_sdk.api.v1_api import V1Api
