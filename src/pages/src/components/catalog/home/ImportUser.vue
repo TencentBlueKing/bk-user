a380f2501f027a1035ffa4681e6a2d3df67c320e
caohua <317282606@qq.com>
2022-12-09 16:36:03 +0800

    feature: 审计日志中新增人员全名列&导出列表优化&通过excel导入用户组织支持全量更新 (#836)
    Co-authored-by: huacao <huacao@canway.net>

--- src/pages/src/components/catalog/home/ImportUser.vue
+++ src/pages/src/components/catalog/home/ImportUser.vue
@@ -48,6 +48,13 @@
       <span class="text">{{$t('仅支持xls、xlsx格式文件')}}</span>
       <span class="template" @click="downloadTemplate"><i class="icon-user-download"></i>{{$t('下载模板')}}</span>
     </p>
+    <p class="tip-wrapper">
+      <bk-checkbox
+        v-model="uploadInfo.isOverwrite">
+        允许对同名用户覆盖更新
+      </bk-checkbox>
+      <i class="tip-icon icon-user--l" v-bk-tooltips="{ width: 310, content: $t('导入覆盖提升') }"></i>
+    </p>
   </div>
 </template>
 
@@ -69,6 +76,7 @@ export default {
         name: '',
         size: '',
         type: false,
+        isOverwrite: false,
       },
     };
   },
@@ -116,6 +124,7 @@ export default {
         name: '',
         size: '',
         type: false,
+        isOverwrite: false,
       };
     },
     // 上传
@@ -305,6 +314,14 @@ export default {
     right: 0;
     color: rgba(99, 101, 110, 1);
     cursor: pointer;
+  }
+}
+
+.tip-wrapper {
+  margin-top: 10px;
+  .tip-icon {
+    display: inline-block;
+    transform: translateY(4px);
   }
 }
 </style>
