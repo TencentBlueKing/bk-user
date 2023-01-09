9a883f109b20dfec360b86130d003a610fdd19c9
v_yutyi <a670003190@qq.com>
2023-01-09 19:24:49 +0800

    opt: 账号有效期优化 & 用户信息组件优化 #834

--- src/pages/src/views/organization/details/InputDate.vue
+++ src/pages/src/views/organization/details/InputDate.vue
@@ -18,11 +18,6 @@
       :options="starttimePickerOptions"
       @change="changeDate">
     </bk-date-picker>
-    <p class="hint" v-show="item.isError">
-      <i class="arrow"></i>
-      <i class="icon-user-exclamation-circle-shape"></i>
-      <span class="text">{{$t('请填写正确')}}{{item.name}}</span>
-    </p>
   </div>
 </template>
 
@@ -74,47 +69,5 @@ export default {
 
 .mark-red .king-date-picker input {
   border: 1px solid #ea3636;
-}
-</style>
-
-<style lang="scss" scoped>
-.hint {
-  padding: 10px;
-  position: absolute;
-  top: -42px;
-  right: 0;
-  color: #fff;
-  font-size: 0;
-  border-radius: 4px;
-  background: rgba(0, 0, 0, .8);;
-
-  .arrow {
-    position: absolute;
-    bottom: -2px;
-    right: 14px;
-    width: 6px;
-    height: 6px;
-    border-top: 1px solid #000;
-    border-left: 1px solid #000;
-    transform: rotate(45deg);
-    z-index: 10;
-    background: #000;
-  }
-
-  .text,
-  .icon-user-exclamation-circle-shape {
-    display: inline-block;
-    vertical-align: middle;
-    font-size: 12px;
-  }
-
-  .icon-user-exclamation-circle-shape {
-    font-size: 16px;
-    margin-right: 10px;
-    position: relative;
-    right: 0;
-    top: 0;
-    color: #fff;
-  }
 }
 </style>
