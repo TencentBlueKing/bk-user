737385287caf6aa3da8247096ff6327d8ec7aa5a
nannan00 <17491932+nannan00@users.noreply.github.com>
2023-07-13 17:50:32 +0800

    feat: minor (#1046)

--- src/bk-user/bkuser/apis/web/data_source/urls.py
+++ src/bk-user/bkuser/apis/web/data_source/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-权限中心(BlueKing-IAM) available.
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-用户管理(Bk-User) available.
 Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
 Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
 You may obtain a copy of the License at http://opensource.org/licenses/MIT
@@ -14,6 +14,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.DataSourceListCreateApi.as_view(), name="category.list_create"),
+    path("", views.DataSourceListCreateApi.as_view(), name="data_source.list_create"),
 ]
 
--- src/bk-user/bkuser/utils/std_error.py
+++ src/bk-user/bkuser/utils/std_error.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
- * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-蓝鲸 PaaS 平台(BlueKing-PaaS) available.
- * Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-用户管理(Bk-User) available.
+Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
 """
 # 参考来源：https://github.com/TencentBlueKing/bkpaas-python-sdk/blob/master/sdks/blue-krill/blue_krill/web/std_error.py
 from typing import Any, Callable, Dict, Optional, Type, TypeVar, Union, overload
