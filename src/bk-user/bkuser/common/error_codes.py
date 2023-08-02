d87c0eacd1f663681a78c4130a22a0ca7ce8e238
schnee <suzh9@mail2.sysu.edu.cn>
2023-08-02 15:27:45 +0800

    feat: add blue-krill (1.2.3)

--- src/bk-user/bkuser/common/error_codes.py
+++ src/bk-user/bkuser/common/error_codes.py
@@ -8,38 +8,38 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from enum import Enum
 from http import HTTPStatus
 
+from blue_krill.data_types.enum import EnumField, StructuredEnum
 from django.utils.translation import gettext_lazy as _
 
 from bkuser.utils.std_error import ErrorCode
 
 
-class ErrorCodeCategoryEnum(Enum):
+class ErrorCodeCategoryEnum(str, StructuredEnum):
     # 400
-    INVALID_ARGUMENT = "INVALID_ARGUMENT"
-    INVALID_REQUEST = "INVALID_REQUEST"
-    OUT_OF_RANGE = "OUT_OF_RANGE"
-    FAILED_PRECONDITION = "FAILED_PRECONDITION"
+    INVALID_ARGUMENT = EnumField("INVALID_ARGUMENT", label=_("参数不符合格式"))
+    INVALID_REQUEST = EnumField("INVALID_REQUEST", label=_("参数符合格式但不符合业务规则"))
+    OUT_OF_RANGE = EnumField("OUT_OF_RANGE", label=_("客户端指定的范围无效"))
+    FAILED_PRECONDITION = EnumField("FAILED_PRECONDITION", label=_("当前系统状态无法执行请求"))
     # 401
-    UNAUTHENTICATED = "UNAUTHENTICATED"
+    UNAUTHENTICATED = EnumField("UNAUTHENTICATED", label=_("未提供身份认证凭证"))
     # 403
-    IAM_NO_PERMISSION = "IAM_NO_PERMISSION"
-    NO_PERMISSION = "NO_PERMISSION"
+    IAM_NO_PERMISSION = EnumField("IAM_NO_PERMISSION", label=_("权限中心没有相关权限"))
+    NO_PERMISSION = EnumField("NO_PERMISSION", label=_("没有相关权限"))
     # 404
-    NOT_FOUND = "NOT_FOUND"
+    NOT_FOUND = EnumField("NOT_FOUND", label=_("资源不存在"))
     # 409
-    ALREADY_EXISTS = "ALREADY_EXISTS"
-    ABORTED = "ABORTED"
+    ALREADY_EXISTS = EnumField("ALREADY_EXISTS", label=_("客户端尝试创建的资源已存在"))
+    ABORTED = EnumField("ABORTED", label=_("并发冲突（读取/修改/写入冲突）"))
     # 429
-    RATE_LIMIT_EXCEED = "RATE_LIMIT_EXCEED"
-    RESOURCE_EXHAUSTED = "RESOURCE_EXHAUSTED"
+    RATE_LIMIT_EXCEED = EnumField("RATE_LIMIT_EXCEED", label=_("超过频率限制"))
+    RESOURCE_EXHAUSTED = EnumField("RESOURCE_EXHAUSTED", label=_("资源配额不足"))
     # 500
-    INTERNAL = "INTERNAL"
-    UNKNOWN = "UNKNOWN"
+    INTERNAL = EnumField("INTERNAL", label=_("服务器内部错误"))
+    UNKNOWN = EnumField("UNKNOWN", label=_("服务器未知错误"))
     # 501
-    NOT_IMPLEMENTED = "NOT_IMPLEMENTED"
+    NOT_IMPLEMENTED = EnumField("NOT_IMPLEMENTED", label=_("功能未实现"))
 
 
 class ErrorCodes:
@@ -47,23 +47,23 @@ class ErrorCodes:
     INVALID_ARGUMENT = ErrorCode(_("参数非法"))
     UNAUTHENTICATED = ErrorCode(
         _("未认证"),
-        code_category=ErrorCodeCategoryEnum.UNAUTHENTICATED.value,
+        code_category=ErrorCodeCategoryEnum.UNAUTHENTICATED,
         status_code=HTTPStatus.UNAUTHORIZED,
     )
     NO_PERMISSION = ErrorCode(
         _("无权限"),
-        code_category=ErrorCodeCategoryEnum.NO_PERMISSION.value,
+        code_category=ErrorCodeCategoryEnum.NO_PERMISSION,
         status_code=HTTPStatus.FORBIDDEN,
     )
     OBJECT_NOT_FOUND = ErrorCode(
         _("对象未找到"),
-        code_category=ErrorCodeCategoryEnum.NOT_FOUND.value,
+        code_category=ErrorCodeCategoryEnum.NOT_FOUND,
         status_code=HTTPStatus.NOT_FOUND,
     )
     VALIDATION_ERROR = ErrorCode(_("参数校验不通过"))
     SYSTEM_ERROR = ErrorCode(
         _("系统异常"),
-        code_category=ErrorCodeCategoryEnum.INTERNAL.value,
+        code_category=ErrorCodeCategoryEnum.INTERNAL,
         status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
     )
     # 调用外部系统API
