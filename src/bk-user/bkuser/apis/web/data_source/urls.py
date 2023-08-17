e9f9679b956f53fc8b019fb36f6c7d2e8c9ed5cb
郑佩珊[shiisa] <shiisa@canway.net>
2023-08-17 20:02:37 +0800

    feat: list data_source users #1159

--- src/bk-user/bkuser/apis/web/data_source/urls.py
+++ src/bk-user/bkuser/apis/web/data_source/urls.py
@@ -8,12 +8,10 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from django.urls import include, path
+from django.urls import path
+
+from bkuser.apis.web.data_source import views
 
 urlpatterns = [
-    # 基础公共，比如当前登录的用户信息，一些常用常量枚举列表等等
-    path("basic/", include("bkuser.apis.web.basic.urls")),
-    # 租户
-    path("tenants/", include("bkuser.apis.web.tenant.urls")),
-    path("data_source/", include("bkuser.apis.web.data_source.urls")),
+    path("<int:id>/users/", views.DataSourceUserListCreateApi.as_view(), name="data_source.list_create"),
 ]
---
+++ src/bk-user/bkuser/apis/web/data_source/views.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-用户管理(Bk-User) available.
+Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+"""
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import generics, status
+
+from bkuser.apis.web.data_source.serializers import UserSearchInputSLZ, UserSearchOutput
+from bkuser.apps.data_source.models import DataSource, DataSourceUser
+from bkuser.common.error_codes import error_codes
+
+
+class DataSourceUserListCreateApi(generics.ListCreateAPIView):
+    pagination_class = None
+    serializer_class = UserSearchOutput
+
+    @swagger_auto_schema(
+        operation_description="数据源用户列表",
+        query_serializer=UserSearchInputSLZ(),
+        responses={status.HTTP_200_OK: UserSearchOutput(many=True)},
+    )
+    def get(self, request, *args, **kwargs):
+        slz = UserSearchInputSLZ(data=self.request.query_params)
+        slz.is_valid(raise_exception=True)
+        data = slz.validated_data
+        data_source_id = kwargs["id"]
+
+        # 校验数据源是否存在
+        try:
+            data_source = DataSource.objects.get(id=data_source_id)
+        except Exception:
+            raise error_codes.DATA_SOURCE_NOT_EXIST
+
+        self.queryset = DataSourceUser.objects.filter(data_source=data_source)
+
+        if data.get("username"):
+            self.queryset = DataSourceUser.objects.filter(username__icontains=data["username"])
+
+        return self.list(request, *args, **kwargs)
