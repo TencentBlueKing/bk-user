49c6ce6dfd7826cdfe9e1535c3342deb5866ef16
brooklin <brooklin@tencent.com>
2023-08-22 17:50:37 +0800

    feat: 前端工程优化 (#1180)

--- src/pages/.eslintrc.js
+++ src/pages/.eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
   root: true,
   extends: ['@blueking/eslint-config-bk/tsvue3'],
+  plugins: [
+    'simple-import-sort',
+  ],
+  rules: {
+    'simple-import-sort/imports': ['error', {
+      groups: [
+        ['^[a-zA-Z]'],
+        ['^@\\w'],
+        ['^\\.\\.'],
+        ['^\\.'],
+      ],
+    }],
+    'no-param-reassign': 'off',
+  },
   parserOptions: {
-    project: [
-      './tsconfig.json',
-    ],
+    project: 'tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
   },
 };
