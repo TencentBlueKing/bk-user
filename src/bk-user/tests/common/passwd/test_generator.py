df9566d695acdd831402e7c16be6f6334f3880d5
schnee <suzh9@mail2.sysu.edu.cn>
2023-08-25 17:44:29 +0800

    feat: add stringify_pydantic_error util

--- src/bk-user/tests/common/passwd/test_generator.py
+++ src/bk-user/tests/common/passwd/test_generator.py
@@ -8,7 +8,8 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
-from bkuser.common.passwd import PasswordGenerator
+import pytest
+from bkuser.common.passwd import PasswordGenerateError, PasswordGenerator
 
 
 class TestPasswordGenerator:
@@ -23,3 +24,11 @@ class TestPasswordGenerator:
         # PasswordGenerator 内置 PasswordValidator，因此此处检查确实生成了密码即可
         password = PasswordGenerator(simple_passwd_rule).generate()
         assert password != ""
+
+    def test_generate_with_bad_passwd_rule(self, strict_passwd_rule):
+        """限制只使用六位的密码，但是有诸多的限制，导致无法生成可行的密码"""
+        strict_passwd_rule.min_length = 6
+        strict_passwd_rule.max_length = 6
+
+        with pytest.raises(PasswordGenerateError):
+            PasswordGenerator(strict_passwd_rule).generate()
