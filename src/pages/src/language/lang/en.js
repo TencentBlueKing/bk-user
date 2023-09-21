9a558f7ff95e01143d516777d1c193454a5f7cd9
yuri0528 <72436066+yuri0528@users.noreply.github.com>
2023-09-21 19:20:37 +0800

    fix: 2.5.4bugfix & magicBox版本升级 (#1251)

--- src/pages/src/language/lang/en.js
+++ src/pages/src/language/lang/en.js
@@ -574,4 +574,5 @@ export default {
   '确认要还原当前目录？': 'Restore the current directory?',
   无该应用访问权限: 'No access permission for the application',
   返回上一页: 'Return to previous page',
+  新建目录: 'New directory',
 };
