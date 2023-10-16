ab88e15a21c2908b07cdbbae28ae1a3e20f06364
nannan00 <17491932+nannan00@users.noreply.github.com>
2023-10-16 11:13:11 +0800

    fix: load idp plugin in diff project

--- src/idp-plugins/idp_plugins/__init__.py
+++ src/idp-plugins/idp_plugins/__init__.py
@@ -16,10 +16,10 @@ from pathlib import Path
 def load_plugins():
     """加载插件"""
     # 插件目录(即当前目录)
-    plugin_base_dir = Path(__file__).resolve().parent
+    # Note: 这里不能使用Path(__file__).resolve()，因为resolve后会指向软链接目标目录，而非当前目录的真实目录
+    plugin_base_dir = Path(__file__).parent
     # 当前包
     package = f"{plugin_base_dir.parent.name}.{plugin_base_dir.name}"
-
     # 子目录列表（即各个插件目录名）
     sub_dirs = [name for name in os.listdir(plugin_base_dir) if os.path.isdir(plugin_base_dir / name)]
     # 遍历加载各个插件
