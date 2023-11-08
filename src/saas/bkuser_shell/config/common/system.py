1e11b4c4b871f5624c4ccde54ad24c3362ba3318
nannan00 <17491932+nannan00@users.noreply.github.com>
2023-11-08 16:08:34 +0800

    bugfix: 修复gunicorn部署，日志分割异常

--- src/saas/bkuser_shell/config/common/system.py
+++ src/saas/bkuser_shell/config/common/system.py
@@ -42,8 +42,8 @@ VERSION_FILE = "RELEASE.yaml"
 ###################
 # Footer & Header #
 ###################
-BK_DOC_URL = "https://bk.tencent.com/docs/markdown/用户管理/产品白皮书/产品简介/README.md"
-
+BK_DOCS_URL_PREFIX = env("BK_DOCS_URL_PREFIX", default='https://bk.tencent.com/docs')
+BK_DOC_URL = f"{BK_DOCS_URL_PREFIX}/markdown/UserManage/UserGuide/Introduce/README.md"
 
 # ==============================================================================
 # Proxy http connections
