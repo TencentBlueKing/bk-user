306edf2ed760239b4ddefafc94ea6db0a56ea435
schnee <suzh9@mail2.sysu.edu.cn>
2023-12-14 09:48:03 +0800

    test: update unittest

--- src/bk-user/tests/apis/web/organization/test_organization.py
+++ src/bk-user/tests/apis/web/organization/test_organization.py
@@ -9,10 +9,76 @@ an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express o
 specific language governing permissions and limitations under the License.
 """
 import pytest
+from bkuser.apps.data_source.models import DataSourceDepartment
 from bkuser.apps.tenant.models import TenantDepartment
 from django.urls import reverse
 
 pytestmark = pytest.mark.django_db
+
+
+class TestTenantListApi:
+    def test_list(self, api_client, full_data_source):
+        resp = api_client.get(reverse("organization.tenant.list"))
+
+        tenants = resp.data
+        assert len(tenants) == 1
+
+        default_tenant = tenants[0]
+        assert default_tenant["id"] == "default"
+        assert default_tenant["departments"][0]["name"] == "公司"
+
+    # TODO 补充存在协同租户的情况
+
+
+class TestTenantUserListApi:
+    def test_list(self, api_client, random_tenant, full_data_source):
+        resp = api_client.get(
+            reverse("organization.tenant.users.list", kwargs={"id": random_tenant.id}),
+            data={"page": 1, "page_size": 50},
+        )
+        # 默认租户至少拥有 admin + 数据源同步的用户 * 11
+        assert resp.data["count"] >= 12  # noqa: PLR2004
+        # 由于可能存在随机用户，因此采用 subset 的方式判断是否符合预期
+        tenant_usernames = {u["username"] for u in resp.data["results"]}
+        must_exists_usernames = {
+            "admin",
+            "zhangsan",
+            "lisi",
+            "wangwu",
+            "zhaoliu",
+            "liuqi",
+            "maiba",
+            "yangjiu",
+            "lushi",
+            "linshiyi",
+            "baishier",
+            "freedom",
+        }
+        assert must_exists_usernames.issubset(tenant_usernames)
+
+
+class TestTenantDepartmentChildrenListApi:
+    def test_list(self, api_client, full_data_source):
+        data_source_dept = DataSourceDepartment.objects.get(data_source=full_data_source, code="company")
+        tenant_dept = TenantDepartment.objects.get(data_source_department=data_source_dept)
+        resp = api_client.get(reverse("organization.children.list", kwargs={"id": tenant_dept.id}))
+
+        assert {d["name"] for d in resp.data} == {"部门A", "部门B"}
+        assert all(d["has_children"] for d in resp.data)
+
+    def test_list_sub_dept_not_has_children(self, api_client, full_data_source):
+        data_source_dept = DataSourceDepartment.objects.get(data_source=full_data_source, code="center_aa")
+        tenant_dept = TenantDepartment.objects.get(data_source_department=data_source_dept)
+        resp = api_client.get(reverse("organization.children.list", kwargs={"id": tenant_dept.id}))
+
+        assert {d["name"] for d in resp.data} == {"小组AAA"}
+        assert not any(d["has_children"] for d in resp.data)
+
+    def test_list_not_children_dept(self, api_client, full_data_source):
+        data_source_dept = DataSourceDepartment.objects.get(data_source=full_data_source, code="group_aaa")
+        tenant_dept = TenantDepartment.objects.get(data_source_department=data_source_dept)
+        resp = api_client.get(reverse("organization.children.list", kwargs={"id": tenant_dept.id}))
+        assert resp.data == []
 
 
 class TestTenantDepartmentUserListApi:
@@ -20,7 +86,7 @@ class TestTenantDepartmentUserListApi:
         dept = TenantDepartment.objects.select_related("data_source_department").get(
             data_source=full_data_source, data_source_department__code="company"
         )
-        resp = api_client.get(reverse("departments.users.list", args=[dept.id]))
+        resp = api_client.get(reverse("departments.users.list", kwargs={"id": dept.id}))
 
         assert resp.data["count"] == 1
         user = resp.data["results"][0]
@@ -35,7 +101,7 @@ class TestTenantDepartmentUserListApi:
         dept = TenantDepartment.objects.select_related("data_source_department").get(
             data_source=full_data_source, data_source_department__code="company"
         )
-        resp = api_client.get(reverse("departments.users.list", args=[dept.id]), data={"recursive": True})
+        resp = api_client.get(reverse("departments.users.list", kwargs={"id": dept.id}), data={"recursive": True})
 
         assert resp.data["count"] == 10  # noqa: PLR2004
 
@@ -44,32 +110,10 @@ class TestTenantDepartmentUserListApi:
             data_source=full_data_source, data_source_department__code="company"
         )
         resp = api_client.get(
-            reverse("departments.users.list", args=[dept.id]), data={"recursive": True, "keyword": "lushi"}
+            reverse("departments.users.list", kwargs={"id": dept.id}), data={"recursive": True, "keyword": "lushi"}
         )
 
         assert resp.data["count"] == 1
         user = resp.data["results"][0]
         assert user["username"] == "lushi"
         assert {dept["name"] for dept in user["departments"]} == {"小组ABA", "中心BA"}
-
-
-class TestTenantListApi:
-    def test_list(self, api_client, full_data_source):
-        resp = api_client.get(reverse("organization.tenant.list"))
-
-        tenants = resp.data
-        assert len(tenants) == 1
-
-        default_tenant = tenants[0]
-        assert default_tenant["id"] == "default"
-        assert default_tenant["departments"][0]["name"] == "公司"
-
-        # TODO 补充存在协同租户的情况
-
-
-class TestTenantDepartmentChildrenListApi:
-    pass
-
-
-class TestTenantUserListApi:
-    pass
