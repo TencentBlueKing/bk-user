1955e3a0d90576476e8598d92b07da096980173e
schnee <suzh9@mail2.sysu.edu.cn>
2023-12-21 14:51:26 +0800

    minor: update comments

--- src/bk-user/bkuser/biz/validators.py
+++ src/bk-user/bkuser/biz/validators.py
@@ -43,6 +43,6 @@ def validate_logo(value):
     if not value:
         return
 
-    # logo是个base64字符串，base64数据实际大小 = (实际数据大小) //3 * 4，需要做大小转换
+    # Logo 使用 Base64 编码，编码后长度 ≈ 原始图片字节长度 // 3 * 4 
     if len(value) > (settings.MAX_LOGO_SIZE * 1024) // 3 * 4:
         raise ValidationError(_("Logo 文件大小不可超过 {} KB").format(settings.MAX_LOGO_SIZE))
