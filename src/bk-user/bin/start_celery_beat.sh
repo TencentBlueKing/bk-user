ac9507b7829e0244eca5ff5192e40c0e3f3377ca
schnee <suzh9@mail2.sysu.edu.cn>
2024-02-20 16:57:15 +0800

    fix: check and update celery config (#1565)

--- src/bk-user/bin/start_celery_beat.sh
+++ src/bk-user/bin/start_celery_beat.sh
@@ -4,4 +4,4 @@
 CELERY_LOG_LEVEL=${CELERY_LOG_LEVEL:-info}
 
 # Run!
-celery -A bkuser.celery beat -l ${CELERY_LOG_LEVEL} --scheduler django_celery_beat.schedulers:DatabaseScheduler
+celery -A bkuser.celery beat -l ${CELERY_LOG_LEVEL}
