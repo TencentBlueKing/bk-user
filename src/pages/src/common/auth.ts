4c2326cfbb6c2fc9b46d4a3c2414d7f66c0b9633
brooklin <brooklin@tencent.com>
2024-04-16 10:18:23 +0800

    feat: 登录跳转修复 (#1647)

--- src/pages/src/common/auth.ts
+++ src/pages/src/common/auth.ts
@@ -1,6 +1,6 @@
 // 获取登录地址
 export const getLoginUrl = () => {
-  const successUrl = `${window.location.origin}/static/login_success.html`;
+  const successUrl = `${window.location.origin}/${window.BK_STATIC_URL}/login_success.html`;
   const siteLoginUrl = window.BK_LOGIN_URL || '';
   if (!siteLoginUrl) {
     console.error('Login URL not configured!');
@@ -8,7 +8,7 @@ export const getLoginUrl = () => {
   }
 
   const [loginBaseUrl] = siteLoginUrl.split('?');
-  const newUrl = loginBaseUrl.includes('login/') ? loginBaseUrl : `${loginBaseUrl}login/`;
+  const newUrl = loginBaseUrl.includes('login') ? `${loginBaseUrl}/` : `${loginBaseUrl}login/`;
   const loginUrl = `${newUrl}plain?size=small&c_url=${encodeURIComponent(successUrl)}`;
   return loginUrl;
 };
