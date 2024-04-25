055d903837ceb5583371f40c89dd0e3585ad3acb
yuri0528 <72436066+yuri0528@users.noreply.github.com>
2024-04-25 17:25:19 +0800

    feat: 消息通知 & 版本日志 #1666 (#1673)

--- src/pages/src/http/api.ts
+++ src/pages/src/http/api.ts
@@ -1,3 +1,6 @@
 import http from './fetch';
 
 export const currentUser = () => http.get('/api/v1/web/basic/current-user/');
+
+// 版本日志列表
+export const getVersionLogs = () => http.get('/api/v1/web/version-logs/');
