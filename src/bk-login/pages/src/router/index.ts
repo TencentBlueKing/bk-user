21eda944cfeed731cecf364d041de383076937b7
brooklin <brooklin@tencent.com>
2024-05-10 16:31:05 +0800

    feat: 登录联调 # Reviewed, transaction id: 7507

--- src/bk-login/pages/src/router/index.ts
+++ src/bk-login/pages/src/router/index.ts
@@ -6,6 +6,7 @@ import {
 // const HomeDemo = () => import(/* webpackChunkName: "HomeDemo" */ '../views/home-demo.vue');
 const Home = () => import(/* webpackChunkName: "Home" */ '../views/home.vue');
 const User = () => import(/* webpackChunkName: "Home" */ '../views/user.vue');
+const Admin = () => import(/* webpackChunkName: "Home" */ '../views/admin.vue');
 
 export default createRouter({
   history: createWebHistory(''),
@@ -13,17 +14,22 @@ export default createRouter({
     {
       path: `${window.SITE_URL}/`,
       component: Home,
-      name: 'Login',
+      name: 'login',
     },
     {
       path: `${window.SITE_URL}/plain/`,
       component: Home,
-      name: 'LoginPlain',
+      name: 'login-plain',
+    },
+    {
+      path: `${window.SITE_URL}/admin`,
+      component: Admin,
+      name: 'admin',
     },
     {
       path: `${window.SITE_URL}/page/users/`,
       component: User,
-      name: 'User',
+      name: 'user',
     },
   ],
 });
---
+++ src/bk-login/pages/src/views/components/custom-login.vue
@@ -0,0 +1,44 @@
+<template>
+  <bk-form form-type="vertical">
+    <div class="logo">
+      <img src="../../../static/images/user.png" />
+    </div>
+    <bk-form-item>
+      <bk-button
+        theme="primary"
+        size="large"
+        class="login-btn"
+        @click="handleLogin">
+        立即登录
+      </bk-button>
+    </bk-form-item>
+  </bk-form>
+</template>
+
+<script setup lang="ts">
+import useAppStore from '@/store/app';
+const props = defineProps({
+  idpId: {
+    type: String,
+    default: '',
+  },
+});
+
+const appStore = useAppStore();
+const handleLogin = () => {
+  window.location.href = `${window.SITE_URL}/tenants/${appStore.tenantId}/idps/${props.idpId}/actions/login/`;
+};
+</script>
+
+<style scoped lang="postcss">
+.logo {
+  text-align: center;
+  img {
+    height: 123px;
+  }
+}
+.login-btn {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
