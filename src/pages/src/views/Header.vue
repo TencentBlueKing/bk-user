8e05b44af89c2a498ce3796da706822ff11c1e85
yzygyin <168320989+yzygyin@users.noreply.github.com>
2024-05-11 17:17:26 +0800

    feat: 空页面开发 (#1700)

--- src/pages/src/views/Header.vue
+++ src/pages/src/views/Header.vue
@@ -20,8 +20,8 @@
           <span class="title-desc">{{ $t('蓝鲸用户管理') }}</span>
         </div>
         <div class="tenant-style" v-if="!isTenant">
-          <span class="logo">{{ logoConvert(userStore.user?.tenant_id) }}</span>
-          <span class="tenant-id">{{ userStore.user?.tenant_id }}</span>
+          <span class="logo">{{ userData?.logo || logoConvert(userData?.name) }}</span>
+          <bk-overflow-title type="tips" class="tenant-id">{{ userData?.name }}</bk-overflow-title>
           <i
             v-if="userStore.user?.role === 'super_manager'"
             class="user-icon icon-shezhi"
@@ -129,7 +129,7 @@
 <script setup lang="ts">
 import { DownShape } from 'bkui-vue/lib/icon';
 import Cookies from 'js-cookie';
-import { computed, reactive, ref } from 'vue';
+import { computed, reactive, ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 
 import NoticeComponent from '@blueking/notice-component';
@@ -143,6 +143,7 @@ import I18n, { t } from '@/language/index';
 import router from '@/router';
 import { useUser } from '@/store';
 import { logoConvert } from '@/utils';
+import { getVersionLogs, getTenantInfo } from '@/http';
 
 const state = reactive({
   logoutDropdown: false,
@@ -219,6 +220,14 @@ const toTenant = () => {
   router.push({ name: 'tenant' });
   headerNav.value = [];
 };
+const userData = ref({});
+const initTenantInfo = async () => {
+  const res = await getTenantInfo();
+  userData.value = res.data;
+};
+onMounted(() => {
+  initTenantInfo();
+});
 
 const onGoBack = () => {
   const { role } = userStore.user;
@@ -304,6 +313,7 @@ const openVersionLog = async () => {
         .tenant-id {
           margin: 0 8px 0 4px;
           color: #FFF;
+          max-width: 150px;
         }
 
         .icon-shezhi {
