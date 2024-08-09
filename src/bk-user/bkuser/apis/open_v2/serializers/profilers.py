dda458f3fd065ca23c4c1df8137e94be885c4670
nannan00 <17491932+nannan00@users.noreply.github.com>
2024-08-09 20:19:59 +0800

    fix(open api v2): profiles list support lookup_field default value - username

--- src/bk-user/bkuser/apis/open_v2/serializers/profilers.py
+++ src/bk-user/bkuser/apis/open_v2/serializers/profilers.py
@@ -8,6 +8,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+
 from typing import List
 
 from rest_framework import serializers
@@ -84,6 +85,7 @@ class ProfileListInputSLZ(ProfileFieldsSLZ):
             "create_time",
         ],
         required=False,
+        default="username",
     )
     exact_lookups = StringArrayField(help_text="精确匹配字段", required=False)
     fuzzy_lookups = StringArrayField(help_text="模糊匹配字段", required=False)
