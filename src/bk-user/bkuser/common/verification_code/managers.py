3c198cf313f4809df7a7e9454c2a69013c0febe2
rolinchen <rolinchen@tencent.com>
2024-08-21 17:02:49 +0800

    resolve: remove commented code

--- src/bk-user/bkuser/common/verification_code/managers.py
+++ src/bk-user/bkuser/common/verification_code/managers.py
@@ -19,62 +19,6 @@ from bkuser.common.cache import Cache, CacheEnum, CacheKeyPrefixEnum
 from bkuser.common.verification_code.constants import VerificationCodeScene
 from bkuser.common.verification_code.exceptions import GenerateCodeTooFrequently, InvalidVerificationCode
 
-# class VerificationCodeManager:
-#     """手机短信验证码"""
-
-#     lock_timeout = 60
-
-#     def __init__(self, phone: str, phone_country_code: str, scene: VerificationCodeScene):
-#         self.phone = phone
-#         self.phone_country_code = phone_country_code
-#         self.scene = scene
-#         self.cache = Cache(CacheEnum.REDIS, CacheKeyPrefixEnum.VERIFICATION_CODE)
-#         self.retries_cache_key = self._gen_cache_key("retries")
-#         self.code_cache_key = self._gen_cache_key("code")
-#         self.lock_cache_key = self._gen_cache_key("lock")
-
-#     def validate(self, code: str):
-#         """校验验证码是否正确"""
-#         if not self._can_retries():
-#             raise InvalidVerificationCode(_("超过验证码重试次数"))
-
-#         if self.cache.get(self.code_cache_key) != code:
-#             raise InvalidVerificationCode(_("验证码错误或已失效"))
-
-#         # 校验通过后，需要清理掉验证码避免二次使用
-#         self.cache.delete(self.code_cache_key)
-#         self.cache.delete(self.retries_cache_key)
-
-#     def _can_retries(self) -> bool:
-#         """检查当前验证码试错次数"""
-#         retries = self.cache.get(self.retries_cache_key, 0)
-#         if retries < settings.VERIFICATION_CODE_MAX_RETRIES:
-#             self.cache.set(self.retries_cache_key, retries + 1, timeout=settings.VERIFICATION_CODE_VALID_TIME)
-#             return True
-
-#         self.cache.delete(self.code_cache_key)
-#         return False
-
-#     def gen_code(self) -> str:
-#         # 生成验证码有频率限制，不能短时间内频繁生成
-#         if self.cache.get(self.lock_cache_key):
-#             raise GenerateCodeTooFrequently(_("生成验证码过于频繁，请稍后再试"))
-
-#         self.cache.set(self.lock_cache_key, True, timeout=self.lock_timeout)
-
-#         # 验证码字符集：数字，大小写字母
-#         charset = string.ascii_letters + string.digits
-#         code = "".join(secrets.choice(charset) for _ in range(settings.VERIFICATION_CODE_LENGTH))
-#         # 刷新缓存中的验证码
-#         self.cache.set(self.code_cache_key, code, timeout=settings.VERIFICATION_CODE_VALID_TIME)
-#         # 重置试错次数
-#         self.cache.delete(self.retries_cache_key)
-#         return code
-
-#     def _gen_cache_key(self, key_type: str) -> str:
-#         """生成验证码缓存 key"""
-#         return f"{self.scene.value}:{self.phone_country_code}:{self.phone}:{key_type}"
-
 
 class BaseVerificationCodeManager:
     """验证码管理基类"""
