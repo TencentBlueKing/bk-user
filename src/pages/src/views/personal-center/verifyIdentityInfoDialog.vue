e21617ead4d6f93d6c7328704b9d2225bc10c3cf
v_hhwhhuang <v_hhwhhuang@tencent.com>
2024-09-10 10:57:06 +0800

    feat:验证码错误不再直接关闭dialog，去除修改、验证失败的infobox # Reviewed, transaction id: 17875

--- src/pages/src/views/personal-center/verifyIdentityInfoDialog.vue
+++ src/pages/src/views/personal-center/verifyIdentityInfoDialog.vue
@@ -248,8 +248,7 @@ const handleSubmitVerifyForm = async () => {
       });
       infoBoxConfig.title = t('邮箱验证成功');
     } catch (err) {
-      // infoBoxConfig.title = t('邮箱验证失败');
-      // infoBoxConfig.type = fail;
+      infoBoxConfig.type = fail;
     }
   }
   if (type === phone) {
@@ -263,8 +262,7 @@ const handleSubmitVerifyForm = async () => {
       });
       infoBoxConfig.title = t('手机号验证成功');
     } catch (err) {
-      // infoBoxConfig.title = t('手机号验证失败');
-      // infoBoxConfig.type = fail;
+      infoBoxConfig.type = fail;
     }
   }
   submitBtnLoading.value = false;
