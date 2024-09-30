ba70a588ed07563d8fdbad824d0379193c3eaa4b
JoJohw <3096241994@qq.com>
2024-09-30 18:37:44 +0800

    fix: 修复其他数据源同步失败仍轮询的问题 # Reviewed, transaction id: 19811

--- src/pages/src/hooks/useDataSource.ts
+++ src/pages/src/hooks/useDataSource.ts
@@ -113,10 +113,10 @@ export const useDataSource = () => {
   };
 
   const stopOperationPollingRule = (data) => {
-    if (data.status === 'success') {
+    if (data.status === 'success' || data.status === 'failed') {
       stopPolling();
     }
-  }
+  };
 
   const importDataTimePolling = ref(null);
 
