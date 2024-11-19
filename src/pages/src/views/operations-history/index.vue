5636065ffa9abf8effc282e03080c626b36a1422
JoJohw <3096241994@qq.com>
2024-11-19 10:03:31 +0800

    feat: 优化折叠效果

--- src/pages/src/views/operations-history/index.vue
+++ src/pages/src/views/operations-history/index.vue
@@ -1,64 +1,65 @@
 <template>
   <div class="grid place-items-center">
     <!-- 筛选表单 -->
-    <div class="filter-operation-history-container" v-if="!isFold">
-      <bk-form class="w-full" form-type="vertical">
-        <bk-form-item class="inline-block ml-[24px]" :label="$t('操作人')">
-          <bk-input class="items-input" clearable />
-        </bk-form-item>
-        <bk-form-item class="inline-block ml-[20px]" :label="$t('操作类型')">
-          <bk-select class="items-select">
-          </bk-select>
-        </bk-form-item>
-        <bk-form-item class="inline-block ml-[20px]" :label="$t('操作对象')">
-          <bk-select class="items-select">
-          </bk-select>
-        </bk-form-item>
-        <bk-form-item class="inline-block ml-[20px]" :label="$t('操作实例')">
-          <bk-input class="items-input" clearable />
-        </bk-form-item>
-        <bk-form-item class="inline-block ml-[24px]" :label="$t('操作时间')">
-          <bk-date-picker class="items-picker" />
-        </bk-form-item>
-        <bk-form-item class="inline-block ml-[20px]" :label="$t('操作结果')">
-          <bk-select class="items-select">
-          </bk-select>
-        </bk-form-item>
-        <bk-form-item class="ml-[24px]">
-          <bk-button
-            class="w-[88px] h-[32px]"
-            theme="primary"
-            @click="() => {}"
-          >
-            {{ $t('查询') }}
-          </bk-button>
-          <bk-button
-            class="w-[88px] h-[32px] ml-[8px]"
-            @click="() => {}"
-          >
-            {{ $t('重置') }}
-          </bk-button>
-        </bk-form-item>
-
-      </bk-form>
-    </div>
+    <transition>
+      <div class="filter-operation-history-container" v-if="!isFold">
+        <bk-form class="w-full mt-[24px]" form-type="vertical">
+          <bk-form-item class="inline-block ml-[24px]" :label="$t('操作人')">
+            <bk-input class="items-input" clearable />
+          </bk-form-item>
+          <bk-form-item class="inline-block ml-[20px]" :label="$t('操作类型')">
+            <bk-select class="items-select">
+            </bk-select>
+          </bk-form-item>
+          <bk-form-item class="inline-block ml-[20px]" :label="$t('操作对象')">
+            <bk-select class="items-select">
+            </bk-select>
+          </bk-form-item>
+          <bk-form-item class="inline-block ml-[20px]" :label="$t('操作实例')">
+            <bk-input class="items-input" clearable />
+          </bk-form-item>
+          <bk-form-item class="inline-block ml-[24px]" :label="$t('操作时间')">
+            <bk-date-picker class="items-picker" />
+          </bk-form-item>
+          <bk-form-item class="inline-block ml-[20px]" :label="$t('操作结果')">
+            <bk-select class="items-select">
+            </bk-select>
+          </bk-form-item>
+          <bk-form-item class="ml-[24px]">
+            <bk-button
+              class="w-[88px] h-[32px]"
+              theme="primary"
+              @click="() => {}"
+            >
+              {{ $t('查询') }}
+            </bk-button>
+            <bk-button
+              class="w-[88px] h-[32px] ml-[8px]"
+              @click="() => {}"
+            >
+              {{ $t('重置') }}
+            </bk-button>
+          </bk-form-item>
+        </bk-form>
+      </div>
+    </transition>
     <!-- 折叠按钮 -->
     <div class="flex justify-center w-full">
       <div @mouseover="handleHoverFoldBtn" @mouseleave="handleLeaveFoldBtn" v-if="!isFold">
         <bk-button
           v-if="isHover" theme="primary"
           @click="toggleFold"
-          class="w-[120px] h-[24px] text-bold">︿</bk-button>
+          class="w-[120px] h-[24px] text-bold border-none">︿</bk-button>
         <bk-button
           v-else
           @click="toggleFold"
-          class="w-[120px] h-[24px] !bg-[#DCDEE5] !text-[#ffffff] text-bold">︿</bk-button>
+          class="w-[120px] h-[24px] !bg-[#DCDEE5] !text-[#ffffff] text-bold border-none">︿</bk-button>
       </div>
       <div v-else>
         <bk-button
           theme="primary"
           @click="toggleFold"
-          class="w-[120px] h-[24px] text-bold">﹀</bk-button>
+          class="w-[120px] h-[24px] text-bold border-none">﹀</bk-button>
       </div>
     </div>
     <!-- 展示列表 -->
@@ -116,10 +117,10 @@ const pageCurrentChange = () => {
 .filter-operation-history-container {
   width: @container-width;
   height: 248px;
-  padding: 24px 0px;
   background: #FFFFFF;
   box-shadow: 0 2px 4px 0 #1919290d;
   border-radius: 2px;
+  overflow: hidden;
 
   .items-input, .items-select, .items-picker {
     width: 298px;
@@ -128,5 +129,15 @@ const pageCurrentChange = () => {
 
 .data-operation-history-container {
   width: @container-width;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: height 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  height: 0px;
 }
 </style>
