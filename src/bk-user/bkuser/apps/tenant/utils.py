d04b78fb8cb3293808266f5dcc1e891f9ce93048
rolinchen <rolinchen@tencent.com>
2024-12-16 21:20:16 +0800

    feat: added generation rule for tenant_user_id by nanoid

--- src/bk-user/bkuser/apps/tenant/utils.py
+++ src/bk-user/bkuser/apps/tenant/utils.py
@@ -21,6 +21,7 @@ from typing import Dict, Tuple
 from bkuser.apps.data_source.models import DataSource, DataSourceDepartment, DataSourceUser
 from bkuser.apps.tenant.constants import TenantUserIdRuleEnum
 from bkuser.apps.tenant.models import TenantDepartmentIDRecord, TenantUserIDGenerateConfig, TenantUserIDRecord
+from bkuser.utils.nanoid import generate_nanoid
 from bkuser.utils.uuid import generate_uuid
 
 logger = logging.getLogger(__name__)
@@ -30,7 +31,7 @@ def is_username_frozen(data_source: DataSource) -> bool:
     """数据源用户名是否不可变更"""
     return (
         TenantUserIDGenerateConfig.objects.filter(data_source=data_source)
-        .exclude(rule=TenantUserIdRuleEnum.UUID4_HEX)
+        .exclude(rule__in=[TenantUserIdRuleEnum.NANOID, TenantUserIdRuleEnum.UUID4_HEX])
         .exists()
     )
 
@@ -62,7 +63,7 @@ class TenantUserIDGenerator:
     def gen(self, user: DataSourceUser) -> str:
         """生成租户用户 ID"""
         if not self.cfg:
-            return self._reuse_or_generate_uuid(user)
+            return self._reuse_or_generate_id(user)
 
         if self.cfg.rule == TenantUserIdRuleEnum.USERNAME_WITH_DOMAIN:
             return f"{user.username}@{self.cfg.domain}"
@@ -70,9 +71,11 @@ class TenantUserIDGenerator:
         if self.cfg.rule == TenantUserIdRuleEnum.USERNAME:
             return user.username
 
-        return self._reuse_or_generate_uuid(user)
+        return self._reuse_or_generate_id(user, self.cfg.rule)
 
-    def _reuse_or_generate_uuid(self, user: DataSourceUser) -> str:
+    def _reuse_or_generate_id(
+        self, user: DataSourceUser, rule: TenantUserIdRuleEnum = TenantUserIdRuleEnum.NANOID
+    ) -> str:
         if self.prepare_batch:
             # 有准备的，直接从映射表里面查询
             if user_id := self.tenant_user_id_map.get((self.target_tenant_id, self.data_source.id, user.code)):
@@ -85,11 +88,14 @@ class TenantUserIDGenerator:
             if record and record.tenant_user_id:
                 return record.tenant_user_id
 
-        uuid = generate_uuid()
+        generated_id = generate_uuid() if rule == TenantUserIdRuleEnum.UUID4_HEX else generate_nanoid()
         TenantUserIDRecord.objects.create(
-            tenant_id=self.target_tenant_id, data_source_id=self.data_source.id, code=user.code, tenant_user_id=uuid
+            tenant_id=self.target_tenant_id,
+            data_source_id=self.data_source.id,
+            code=user.code,
+            tenant_user_id=generated_id,
         )
-        return uuid
+        return generated_id
 
 
 class TenantDeptIDGenerator:
---
+++ src/bk-user/bkuser/utils/nanoid.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# TencentBlueKing is pleased to support the open source community by making
+# 蓝鲸智云 - 用户管理 (bk-user) available.
+# Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We undertake not to change the open source license (MIT license) applicable
+# to the current version of the project delivered to anyone in the future.
+
+from nanoid import generate
+
+
+def generate_nanoid(size: int = 16) -> str:
+    # 字符集由 0-9 的阿拉伯数字与小写字母组成，默认长度为 16
+    return generate(alphabet="0123456789abcdefghijklmnopqrstuvwxyz", size=size)
