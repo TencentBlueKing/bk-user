5fdf9a4603de76f03d0030a21bfded67f088bd30
rolin999 <rolinchen@tencent.com>
2024-12-16 16:38:29 +0800

    feat: added filter by departments for list profiles (#2015)

--- src/bk-user/bkuser/apis/open_v2/serializers/profilers.py
+++ src/bk-user/bkuser/apis/open_v2/serializers/profilers.py
@@ -89,6 +89,8 @@ class ProfileListInputSLZ(ProfileFieldsSLZ):
             "staff_status",
             # IAM 特有
             "create_time",
+            # 关联关系
+            "departments",
         ],
         required=False,
         default="username",
---
+++ src/bk-user/tests/apis/open_v2/test_profiles.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# TencentBlueKing is pleased to support the open source community by making
+# 蓝鲸智云 - 用户管理 (bk-user) available.
+# Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We undertake not to change the open source license (MIT license) applicable
+# to the current version of the project delivered to anyone in the future.
+
+import pytest
+from bkuser.apps.tenant.models import TenantDepartment
+from django.urls import reverse
+from rest_framework import status
+
+pytestmark = pytest.mark.django_db
+
+
+class TestListProfiles:
+    def test_standard(self, api_client, local_data_source, collaboration_data_source):
+        resp = api_client.get(reverse("open_v2.list_profiles"), data={"page": 1, "page_size": 10})
+        assert resp.status_code == status.HTTP_200_OK
+        assert resp.data["count"] == 22
+        assert len(resp.data["results"]) == 10
+
+    def test_list_with_exist_departments(self, api_client, local_data_source, collaboration_data_source):
+        department_ids = TenantDepartment.objects.values_list("id", flat=True)
+        resp = api_client.get(
+            reverse("open_v2.list_profiles"),
+            data={"lookup_field": "departments", "exact_lookups": ",".join(map(str, department_ids))},
+        )
+        assert resp.status_code == status.HTTP_200_OK
+        assert resp.data["count"] == 20
+
+    def test_list_with_non_exist_departments(self, api_client, local_data_source, collaboration_data_source):
+        resp = api_client.get(
+            reverse("open_v2.list_profiles"),
+            data={"lookup_field": "departments", "exact_lookups": "9999"},
+        )
+        assert resp.status_code == status.HTTP_200_OK
+        assert resp.data["count"] == 0
+
+    def test_list_with_invalid_fuzzy_lookups(self, api_client, local_data_source, collaboration_data_source):
+        resp = api_client.get(
+            reverse("open_v2.list_profiles"),
+            data={"lookup_field": "departments", "fuzzy_lookups": "1"},
+        )
+        assert resp.status_code == status.HTTP_400_BAD_REQUEST
+        assert "unsupported fuzzy lookup field: departments" in resp.data["message"]
