c9a454c0ec5327f05521c6bfee4dd3bc2eecfd68
JoJo_ <92574547+JoJohw@users.noreply.github.com>
2025-04-15 17:27:18 +0800

    feat: 环境变量字段替换 (#2080)

--- src/pages/src/utils/index.ts
+++ src/pages/src/utils/index.ts
@@ -379,7 +379,7 @@ export const tenantStatus = {
 // 语言切换
 export const handleSwitchLocale = async (locale: string, tenant_id: string) => {
   try {
-    const url = `${window.BK_COMPONENT_API_URL}/api/bk-user-web/prod/api/v3/open-web/tenant/current-user/language/`;
+    const url = `${window.BK_USER_WEB_APIGW_URL}/api/bk-user-web/prod/api/v3/open-web/tenant/current-user/language/`;
     await fetch(url, {
       method: 'PUT',
       headers: {
