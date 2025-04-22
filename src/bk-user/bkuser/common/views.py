47d841cdcd6cbbe811961f86fead9a983ab07dff
rolinchen <rolinchen@tencent.com>
2025-04-22 17:52:08 +0800

    fix: modify creator display_name for audit record

--- src/bk-user/bkuser/common/views.py
+++ src/bk-user/bkuser/common/views.py
@@ -201,8 +201,8 @@ class VueTemplateView(TemplateView):
                 "ENABLE_VIRTUAL_USER": settings.ENABLE_VIRTUAL_USER,
                 # 是否启用新建租户功能
                 "ENABLE_CREATE_TENANT": settings.ENABLE_CREATE_TENANT,
-                # 前端服务 API 网关（bk-user-web）URL
-                "BK_USER_WEB_APIGW_URL": settings.BK_API_URL_TMPL.format(api_name="bk-user-web/prod"),
+                # 前端服务 API 网关（bk-user-web）正式环境（prod) URL
+                "BK_USER_WEB_APIGW_URL": f"{settings.BK_API_URL_TMPL.format(api_name='bk-user-web')}/prod/",
             }
 
         except Exception:  # pylint: disable=broad-except
