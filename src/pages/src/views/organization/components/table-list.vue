f50d0dbe411f2fd53e970f0b57a244c5a70049eb
JoJohw <3096241994@qq.com>
2025-05-09 10:42:09 +0800

    fix: 切换pageSize滚动条不重置 # Reviewed, transaction id: 41730

--- src/pages/src/views/organization/components/table-list.vue
+++ src/pages/src/views/organization/components/table-list.vue
@@ -34,7 +34,6 @@
             :thead="{ height: headHeight }"
             :row-height="lineHeight"
             :max-height="curTableMaxHeight"
-            :height="curTableHeight"
             min-height="30%"
             class="organization-table-main"
             :border="['outer']"
@@ -815,20 +814,10 @@ const batchMoveOrg = (params) => {
 const paginationHeight = 60;
 const headHeight = 42;
 const lineHeight = ref(42);
-// 使用max-height来限制表格高度
-const curTableMaxHeight = computed(() => {
-  // header(52) + subHeader(52) + padding(48) + actionBar(32) + actionBarMarginBottom(16) 固定表格最大高度，对当前高度进行响应式计算，纵享丝滑
-  const tableMaxHeight = window.innerHeight - 200;
-  // 计算当前分页下table高度
-  const targetTableHeight = headHeight + lineHeight.value * pagination.limit + paginationHeight;
-  // 若超出页面允许的最大高度，返回最大高度，否则返回数据铺满高度，防止出现空白区域未铺满数据的情况
-  if (targetTableHeight < tableMaxHeight) return targetTableHeight;
-  return tableMaxHeight;
-})
 // table height继续采用响应式hook，用于处理window resize以及折叠功能高度的计算(curRedundantHeight is ref data)
 const dynamicTableHeight = useTableMaxHeight(200);
-const curTableHeight = computed(() => {
-  if (pagination.count < pagination.limit) return 'auto';
+const curTableMaxHeight = computed(() => {
+  if (pagination.count < pagination.limit) return '100%';
   return dynamicTableHeight.value;
 });
 
