c6a49adae6315c647068843c8291c229bca8bbb1
rolinchen <rolinchen@tencent.com>
2025-06-12 19:50:58 +0800

    feat: added crontab for sync data source

--- src/bk-user/bkuser/apps/sync/constants.py
+++ src/bk-user/bkuser/apps/sync/constants.py
@@ -28,7 +28,7 @@ EMAIL_REGEX = re.compile(r"^[\w.-]+@[\w.-]+\.[A-Za-z]{2,6}$")
 class DataSourceSyncPeriodType(StrStructuredEnum):
     """数据源自动同步周期类型（单位）"""
 
-    NEVER = EnumField("never", label=_("永不"))
+    NEVER = EnumField("never", label=_("从不"))
     MINUTE = EnumField("minute", label=_("分钟"))
     HOUR = EnumField("hour", label=_("小时"))
     DAY = EnumField("day", label=_("天"))
