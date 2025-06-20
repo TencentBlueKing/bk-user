e6559a7fde0182831e50e9c30079f8a489bb81da
brooklin <brooklin@tencent.com>
2025-06-20 15:24:01 +0800

    feat: 管理员登录&验收问题修复 (#2159)

--- src/bk-login/pages/src/http/api.ts
+++ src/bk-login/pages/src/http/api.ts
@@ -18,7 +18,7 @@ export const getUserList = () => fetch.get(`${apiPrefix}/tenant-users/`);
 export const signInByUser = (params: UserParams) => fetch.post(`${apiPrefix}/sign-in-users/`, params);
 
 // 搜索租户列表，支持 id 和 name
-export const searchTenantList = (payload: { keyword: string }) => fetch.get(`${apiPrefix}/tenants/-/search`, payload);
+export const getSearchTenantList = (payload: { keyword: string }) => fetch.get(`${apiPrefix}/tenants/-/search`, payload);
 
 // 通过id搜索租户列表
 export const getTenantList = (payload: { tenant_ids: string }) => fetch.get(`${apiPrefix}/tenants/`, payload);
@@ -29,6 +29,10 @@ export const getIdpList = (id: string) => fetch.get(`${apiPrefix}/idps/`, { tena
 // 帐密登录
 export const signInByPassword = (id: string, idpId: string, params: PasswordParams) => fetch
   .post(`${apiPrefix}/auth/idps/${idpId}/actions/authenticate/`, params);
+
+// 管理员登录
+export const signInByAdmin = (idpId: string, params: PasswordParams) => fetch
+  .post(`${apiPrefix}/builtin-management-auth/idps/${idpId}/authenticate/`, params);
 
 // 全局配置
 export const getGlobalSettings = () => fetch.get(`${apiPrefix}/global-settings/`);
