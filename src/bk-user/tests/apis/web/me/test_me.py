ac3c90b714f8f665f014e67beb7665d7d33c7d24
mufenqwq <wangpan910@gmail.com>
2025-06-26 16:09:22 +0800

    feat: add personal virtual user api

--- src/bk-user/tests/apis/web/me/test_me.py
+++ src/bk-user/tests/apis/web/me/test_me.py
@@ -22,10 +22,10 @@ from rest_framework import status
 pytestmark = pytest.mark.django_db
 
 
-class TestMyVirtualUserListApi:
+class TestMeVirtualUserListApi:
     @pytest.mark.usefixtures("_init_virtual_users")
     def test_list_virtual_user(self, api_client):
-        resp = api_client.get(reverse("my_virtual_user.virtual_users.list"))
+        resp = api_client.get(reverse("me.virtual_users.list"))
         assert resp.status_code == status.HTTP_200_OK
         assert set(resp.data["results"][0].keys()) == {
             "id",
@@ -38,14 +38,14 @@ class TestMyVirtualUserListApi:
 
     @pytest.mark.usefixtures("_init_virtual_users")
     def test_list_virtual_user_with_pagination(self, api_client):
-        resp = api_client.get(reverse("my_virtual_user.virtual_users.list"), data={"page": 1, "page_size": 2})
+        resp = api_client.get(reverse("me.virtual_users.list"), data={"page": 1, "page_size": 2})
         assert resp.status_code == status.HTTP_200_OK
         assert len(resp.data["results"]) == 2
         assert resp.data["count"] == 3
 
     @pytest.mark.usefixtures("_init_tenant_users_depts")
     def test_list_virtual_user_empty(self, api_client):
-        resp = api_client.get(reverse("my_virtual_user.virtual_users.list"))
+        resp = api_client.get(reverse("me.virtual_users.list"))
         assert resp.status_code == status.HTTP_200_OK
         assert len(resp.data["results"]) == 0
         assert resp.data["count"] == 0
@@ -53,49 +53,47 @@ class TestMyVirtualUserListApi:
     @pytest.mark.usefixtures("_init_virtual_users")
     def test_list_virtual_user_with_keyword(self, api_client):
         # 测试能匹配当前用户负责的所有虚拟用户
-        resp = api_client.get(reverse("my_virtual_user.virtual_users.list"), data={"keyword": "virtual"})
+        resp = api_client.get(reverse("me.virtual_users.list"), data={"keyword": "virtual"})
         assert resp.status_code == status.HTTP_200_OK
         assert len(resp.data["results"]) == 3
         assert {r["id"] for r in resp.data["results"]} == {"virtual_user_1", "virtual_user_3", "virtual_user_4"}
 
         # 测试只匹配 virtual_user_1
-        resp = api_client.get(reverse("my_virtual_user.virtual_users.list"), data={"keyword": "virtual_user_1"})
+        resp = api_client.get(reverse("me.virtual_users.list"), data={"keyword": "virtual_user_1"})
         assert resp.status_code == status.HTTP_200_OK
         assert len(resp.data["results"]) == 1
         assert resp.data["results"][0]["id"] == "virtual_user_1"
 
         # 测试不匹配情况
-        resp = api_client.get(
-            reverse("my_virtual_user.virtual_users.list"), data={"keyword": "virtual_user_not_exist"}
-        )
+        resp = api_client.get(reverse("me.virtual_users.list"), data={"keyword": "virtual_user_not_exist"})
         assert resp.status_code == status.HTTP_200_OK
         assert len(resp.data["results"]) == 0
         assert resp.data["count"] == 0
 
 
 @pytest.mark.usefixtures("_init_virtual_users")
-class TestMyVirtualUserGetApi:
+class TestMeVirtualUserGetApi:
     def test_get_virtual_user(self, api_client, random_tenant):
-        url = reverse("my_virtual_user.virtual_users.retrieve_update", kwargs={"id": "virtual_user_1"})
+        url = reverse("me.virtual_users.retrieve_update", kwargs={"id": "virtual_user_1"})
         resp = api_client.get(url)
         assert resp.status_code == status.HTTP_200_OK
         assert resp.data["username"] == "virtual_user_1"
 
     def test_get_virtual_user_not_owned(self, api_client):
-        url = reverse("my_virtual_user.virtual_users.retrieve_update", kwargs={"id": "virtual_user_2"})
+        url = reverse("me.virtual_users.retrieve_update", kwargs={"id": "virtual_user_2"})
         resp = api_client.get(url)
         assert resp.status_code == status.HTTP_403_FORBIDDEN
 
     def test_get_virtual_user_not_exists(self, api_client):
-        url = reverse("my_virtual_user.virtual_users.retrieve_update", kwargs={"id": "virtual_user_not_exist"})
+        url = reverse("me.virtual_users.retrieve_update", kwargs={"id": "virtual_user_not_exist"})
         resp = api_client.get(url)
         assert resp.status_code == status.HTTP_404_NOT_FOUND
 
 
 @pytest.mark.usefixtures("_init_virtual_users")
-class TestMyVirtualUserUpdateApi:
+class TestMeVirtualUserUpdateApi:
     def test_update_virtual_user(self, api_client):
-        url = reverse("my_virtual_user.virtual_users.retrieve_update", kwargs={"id": "virtual_user_1"})
+        url = reverse("me.virtual_users.retrieve_update", kwargs={"id": "virtual_user_1"})
         resp = api_client.put(
             url,
             data={"full_name": "虚拟用户的新名字", "app_codes": ["app4", "app5"], "owners": ["zhangsan", "wangwu"]},
@@ -112,7 +110,7 @@ class TestMyVirtualUserUpdateApi:
         ) == {"zhangsan", "wangwu"}
 
     def test_update_virtual_user_not_owned(self, api_client):
-        url = reverse("my_virtual_user.virtual_users.retrieve_update", kwargs={"id": "virtual_user_2"})
+        url = reverse("me.virtual_users.retrieve_update", kwargs={"id": "virtual_user_2"})
         resp = api_client.put(
             url,
             data={"full_name": "虚拟用户的新名字", "app_codes": ["app4", "app5"], "owners": ["zhangsan", "wangwu"]},
@@ -120,7 +118,7 @@ class TestMyVirtualUserUpdateApi:
         assert resp.status_code == status.HTTP_403_FORBIDDEN
 
     def test_update_virtual_user_not_exists(self, api_client):
-        url = reverse("my_virtual_user.virtual_users.retrieve_update", kwargs={"id": "virtual_user_not_exist"})
+        url = reverse("me.virtual_users.retrieve_update", kwargs={"id": "virtual_user_not_exist"})
         resp = api_client.put(
             url,
             data={"full_name": "虚拟用户的新名字", "app_codes": ["app4", "app5"], "owners": ["zhangsan", "wangwu"]},
