e2db78c0212765ac85bb7e28af0d928e0d31b540
mufen <wangpan910@gmail.com>
2025-12-18 16:59:29 +0800

    feat: 默认关闭微信消息推送功能

--- src/bk-user/bkuser/settings.py
+++ src/bk-user/bkuser/settings.py
@@ -780,7 +780,7 @@ HAS_BK_CMSI_APIGW = env.bool("HAS_BK_CMSI_APIGW", default=False)
 # bk-cmsi 网关部署环境
 BK_CMSI_APIGW_STAGE = env.str("BK_CMSI_APIGW_STAGE", "prod")
 # 是否启用微信消息推送功能
-ENABLE_WEIXIN_NOTIFICATION = env.bool("ENABLE_WEIXIN_NOTIFICATION", default=True)
+ENABLE_WEIXIN_NOTIFICATION = env.bool("ENABLE_WEIXIN_NOTIFICATION", default=False)
 # 是否启用协同租户功能
 ENABLE_COLLABORATION_TENANT = env.bool("ENABLE_COLLABORATION_TENANT", default=False)
 # 内置租户管理员 username
