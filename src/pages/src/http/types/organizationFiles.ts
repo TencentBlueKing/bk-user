086f22902dd8ca6af58a2e4a0c6852a90cd724b7
JoJohw <3096241994@qq.com>
2025-12-22 11:39:40 +0800

    feat: 补充departmentList没有的部门

--- src/pages/src/http/types/organizationFiles.ts
+++ src/pages/src/http/types/organizationFiles.ts
@@ -2,31 +2,37 @@
  * 更新租户参数
  */
 export interface UpdateTenantParams {
-  name: string,
-  logo: string,
-  manager_ids: string[],
+  name: string;
+  logo: string;
+  manager_ids: string[];
   feature_flags: {
-    user_number_visible: boolean,
-  },
+    user_number_visible: boolean;
+  };
 }
 
 /**
  * 租户部门下用户列表参数
  */
 export interface DepartmentsListParams {
-  id: string,
-  keyword: string,
-  page: number,
-  pageSize: number,
-  recursive: boolean,
+  id: string;
+  keyword: string;
+  page: number;
+  pageSize: number;
+  recursive: boolean;
 }
 
 /**
  * 租户下用户列表参数
  */
 export interface TenantListParams {
-  id: string,
-  keyword: string,
-  page: number,
-  pageSize: number,
+  id: string;
+  keyword: string;
+  page: number;
+  pageSize: number;
+}
+
+export interface OptionalDepartmentsListData {
+  id: number;
+  name: string;
+  organization_path: string;
 }
