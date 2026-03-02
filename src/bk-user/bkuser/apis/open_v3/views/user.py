3f5a5cc7ed6246da41018d772856698f09445db7
mufen <wangpan910@gmail.com>
2026-03-02 15:18:36 +0800

    fix: fix typo

--- src/bk-user/bkuser/apis/open_v3/views/user.py
+++ src/bk-user/bkuser/apis/open_v3/views/user.py
@@ -300,7 +300,7 @@ class TenantUserListApi(OpenApiCommonMixin, generics.ListAPIView):
 class TenantUserSensitiveInfoListApi(OpenApiCommonMixin, generics.ListAPIView):
     """
     根据 bk_username 批量查询用户敏感信息
-    # Note: 敏感信息不支持协同用户
+    Note: 敏感信息不支持协同用户
     """
 
     pagination_class = None
