8e5977d4c980c48847b528429869491ceec43725
mufen <wangpan910@gmail.com>
2026-04-17 10:06:26 +0800

    fix: fix docs

--- src/bk-user/bkuser/apis/open_web/views/departments.py
+++ src/bk-user/bkuser/apis/open_web/views/departments.py
@@ -195,6 +195,7 @@ class TenantDepartmentUserListApi(OpenWebApiCommonMixin, generics.ListAPIView):
             user_ids = DataSourceDepartmentUserRelation.objects.filter(data_source=data_source).values_list(
                 "user_id", flat=True
             )
+            # TODO: 这里存在比较大的性能问题，如何快速获取无归属部门的用户？
             queryset = queryset.filter(data_source=data_source).exclude(data_source_user_id__in=user_ids)
 
         return queryset.order_by("id")
