3f0a49c908356c17c86602f9d84b99feeca372dd
JoJo_ <92574547+JoJohw@users.noreply.github.com>
2026-06-30 15:26:05 +0800

    feat: 前端替换导航组件 (#2400)

--- src/pages/src/http/types/api.ts
+++ src/pages/src/http/types/api.ts
@@ -1,8 +1,10 @@
 import type { RoleType } from '@/common/constant';
 
 export interface CurrentUser {
-  username: string
-  display_name: string
-  role: RoleType
-  tenant_id: string
+  username: string;
+  display_name: string;
+  role: RoleType;
+  tenant_id: string;
+  time_zone: string;
+  language: string;
 }
