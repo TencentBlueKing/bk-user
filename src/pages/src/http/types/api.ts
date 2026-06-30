f74f0e47025cefe3151d7c99fffac77c0c7b0d65
JoJo_ <92574547+JoJohw@users.noreply.github.com>
2026-06-30 17:17:57 +0800

    feat: 前端支持多语言 (#2399)

--- src/pages/src/http/types/api.ts
+++ src/pages/src/http/types/api.ts
@@ -8,3 +8,21 @@ export interface CurrentUser {
   time_zone: string;
   language: string;
 }
+
+/** 语言代码类型，必定包含 'zh-cn' 和 'en'，同时允许其他语言代码 */
+export type LanguageCode = 'zh-cn' | 'en' | string;
+
+/** 支持的语言项 */
+export interface SupportedLanguage {
+  code: LanguageCode;
+  name: string;
+}
+
+/**
+ * 全局配置返回结果
+ */
+export interface GlobalSettingsResult {
+  bk_user_url: string;
+  unique_enabled_tenant_idp: string | null,
+  supported_languages: SupportedLanguage[];
+}
